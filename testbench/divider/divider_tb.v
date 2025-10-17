edbe47d2102f32af1a2ac06b34ca20dc47765e66
Ben <13207243+ben-j-c@users.noreply.github.com>
2025-10-17 02:18:19 -0400

    Divider may work, need to sleep so I dont want to ruin it

---
+++ testbench/divider/divider_tb.v
@@ -0,0 +1,49 @@
+`include "../../core/riscv/riscv_defs.v"
+`include "../../core/riscv/riscv_divider.v"
+
+module tb;
+	reg           clk_i;
+	reg           rst_i;
+	reg           opcode_valid_i;
+	reg  [ 31:0]  opcode_opcode_i;
+	reg  [ 31:0]  opcode_pc_i;
+	reg           opcode_invalid_i;
+	reg  [  4:0]  opcode_rd_idx_i;
+	reg  [  4:0]  opcode_ra_idx_i;
+	reg  [  4:0]  opcode_rb_idx_i;
+	reg  [ 31:0]  opcode_ra_operand_i;
+	reg  [ 31:0]  opcode_rb_operand_i;
+	wire          writeback_valid_o;
+	wire [ 31:0]  writeback_value_o;
+
+	riscv_divider dut (
+		.clk_i(clk_i),
+		.rst_i(rst_i),
+		.opcode_valid_i(opcode_valid_i),
+		.opcode_opcode_i(opcode_opcode_i),
+		.opcode_pc_i(opcode_pc_i),
+		.opcode_invalid_i(opcode_invalid_i),
+		.opcode_rd_idx_i(opcode_rd_idx_i),
+		.opcode_ra_idx_i(opcode_ra_idx_i),
+		.opcode_rb_idx_i(opcode_rb_idx_i),
+		.opcode_ra_operand_i(opcode_ra_operand_i),
+		.opcode_rb_operand_i(opcode_rb_operand_i),
+		.writeback_valid_o(writeback_valid_o),
+		.writeback_value_o(writeback_value_o)
+	);
+
+	integer seed;
+	integer i;
+
+	initial begin
+		$dumpfile(".divider_tb.vcd");
+		$dumpvars(1, tb);
+
+		clk_i = 0;
+		rst_i = 0;
+		opcode_valid_i = 0;
+		opcode_opcode_i = 0;
+		opcode_pc_i = 0;
+	end
+
+endmodule
